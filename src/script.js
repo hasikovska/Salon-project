af7810297a33fa25b98e822457582c70fc071328
hasikovska <hasikovskama@gmail.com>
2024-04-09 14:27:03 +0200

    add classes to buttons

---
+++ src/script.js
@@ -0,0 +1,6 @@
+setUpPrice(document.querySelector('.price'))
+
+function setUpPrice(serviceSelection){
+  const service = serviceSelection.querySelectorAll('.service')
+  const buttons = serviceSelection.querySelectorAll('.service-selection_btn')
+}
